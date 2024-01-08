bfa86cae7227b78f511392f9d30eab3398cd7eca
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-01-08 15:29:13 +0530

    local storage completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 import { TodoProvider } from './contexts'
@@ -7,8 +7,31 @@ import { TodoProvider } from './contexts'
 function App() {
 
 const [todos, setTodos]=useState([])
+
+const addTodo=(todo)=>{
+  setTodos((prev)=>[{id:Date.now(),...todo},...prev])
+}
+const updateTodo=(id , todo)=>{
+  setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id===id ? todo:prevTodo)))
+}
+const deleteTodo=(id)=>{
+  setTodos((prev)=>prev.filter((prevtodo)=>prev.id!==id))
+}
+const toggleComplete=(id)=>{
+  setTodos((prev)=>prev.map((prevTodo)=>prevTodo===id?{...prevTodo, completed:!prevTodo.completed}:prevTodo))
+}
+useEffect(()=>{
+  const todos=JSON.parse(localStorage.getItem("todos"))
+  if(todos && todos.length>0){
+    setTodos(todos)
+  }
+},[])
+useEffect(()=>{
+       localStorage.setItem("todos", JSON.stringify(todos))
+},[todos])
+
   return (
-    <TodoProvider>
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
     
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
